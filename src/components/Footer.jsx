b183c2c04ce5283148848e2c79b0cc69bf88c2e6
Your Name <leobarrientos02@gmail.com>
2022-02-13 22:38:42 -0500

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
 export const Footer = () => {
+
+  const d = new Date();
   return (
-    <div>
-        <h1>Footer</h1>
+    <div className='text-center p-10 mt-10 border-t dark:border-gray-700 border-gray-200'>
+        <h1>{d.getFullYear()} GoogLeo, Inc.</h1>
     </div>
   )
 }
